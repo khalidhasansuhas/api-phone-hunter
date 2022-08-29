0836995c5922ca4f9e8482ebae9f78930e37be0d
khalidhasansuhas <khalidhasansuhas@gmail.com>
2022-08-30 00:57:28 +0600

    Enter key event handler added

--- js/app.js
+++ js/app.js
@@ -35,12 +35,13 @@ const displayPhones = (phones, dataLimit) =>{
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML=`
         <div class="card h-100 p-4">
-                        <img src="${phone.image}" class="card-img-top" alt="...">
-                        <div class="card-body">
-                          <h5 class="card-title">${phone.phone_name}</h5>
-                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        </div>
-                      </div>
+             <img src="${phone.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <button href="#" class="btn btn-primary">Show Details</button>
+            </div>
+        </div>
         `
         phonesContainer.appendChild(phoneDiv);
 
